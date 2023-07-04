85617ea5a4b5e55953eda4acc652d9ede90b29ef
unknown <1647802159@qq.com>
2023-07-04 11:14:53 +0800

    递增子串

---
+++ AcWing_3748.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        int N;
+        cin >> N;
+        char S[N];
+        int label[N];
+        for (int j = 0; j < N; j++)
+        {
+            cin >> S[j];
+            if (j == 0)
+            {
+                label[j] = 1;
+                continue;
+            }
+            if (S[j] > S[j - 1])
+            {
+                label[j] = 1;
+            }
+            else
+                label[j] = 0;
+        }
+        int num = 1;
+        cout << "Case #" << i+1 << ": 1";
+        for (int j = 1; j < N; j++)
+        {
+            if (label[j] == 1)
+            {
+                num++;
+            }
+            else
+            {
+                num = 1;
+            }
+            cout << " " << num;
+        }
+        cout << "\n";
+    }
+}
